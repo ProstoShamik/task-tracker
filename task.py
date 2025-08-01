0a45299d43bef03feb34eaccd1b40fbf9b620f8b
eidillum <prostoshamiktv@gmail.com>
2025-08-02 00:50:56 +0500

    some updates

--- task.py
+++ task.py
@@ -1,2 +1,38 @@
+from datetime import datetime
+from enum import Enum
+import random
+import uuid
+from typing import Optional
 
-class 
+class TaskStatus(Enum):
+    NOT_DONE = 'not done'
+    DONE = 'done'
+
+class Task:
+    def __init__(self, title: str, 
+                 description: str, 
+                 created_at: Optional[datetime] = None, 
+                 status = TaskStatus.NOT_DONE, 
+                 finished_at: Optional[datetime] = None, 
+                 id: Optional[str] = None):
+        self.title = title
+        self.description = description
+        self.created_at = created_at or str(datetime.now)
+        self.status = status
+        self.finished_at = finished_at
+        self.id = id or str(uuid.uuid4())[:9]
+
+
+    def __str__(self):
+        return f'''title: {self.title}; description: {self.description}; created_at: {self.created_at}; status: {self.status.value}{f"; finished_at: {self.finished_at}" if self.finished_at else ''}'''
+    
+    def change_title(self, new_title):
+        self.title = new_title
+
+    def change_description(self, new_description):
+        self.description = new_description
+
+    def change_status(self, new_status):
+        self.status = new_status
+
+
--- task_db.py
+++ task_db.py
@@ -1,20 +1,26 @@
 import json
+import task
 
 class Task_DB:
 
     def __init__(self):
-        db = self.__load_db()
+        db = self.__load_db_from_json()
 
     
-    def __load_db(self) -> list:
+    def __load_db_from_json(self) -> list:
         try:
             with open('data/task_db.json', 'r') as file:
-                self.db = json.load(file)
+                tasks_json = json.load(file)
+                for task in tasks_json:
+                    
         except:
             print('''Can't load task_db.json''')
             return []
+        
 
-        for 
+
+
+        
         
         
 
